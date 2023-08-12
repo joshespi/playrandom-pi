ddc332216f3ea47c7166919b3b561ad6a04794e4
Espi <joshespi@gmail.com>
2023-08-12 12:46:14 -0600

    Update tv_control.sh
    modified to run throughout the day on the weekends, but only run in the afternoon during the week.

--- tv_control.sh
+++ tv_control.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 current_hour=$(date +'%H')
+day_of_week=$(date +'%u') # 1 (Monday) through 7 (Sunday)
 
-# Check if the TV should be off based on the current hour
-if [[ $current_hour -ge 22 || $current_hour -lt 6 ]]; then
+# Check if the TV should be off based on the current hour and day of the week
+if [[ ($day_of_week -eq 6 || $day_of_week -eq 7) && ($current_hour -ge 7 && $current_hour -lt 21) ]] ||
+   [[ ($day_of_week -ge 1 && $day_of_week -le 5) && ($current_hour -ge 16 && $current_hour -lt 21) ]]; then
   echo 'standby 0.0.0.0' | cec-client -s -d 1
 else
   echo 'as 0.0.0.0' | cec-client -s -d 1
 fi
-
